535055ec9bd46fc3592aae7c73f5b92dc67b56eb
Dan Kerimdzhanov <kerimdzhanov@gmail.com>
2023-09-29 18:28:07 +0100

    feat(dotenv-flow-webpack): add more use case configs

--- webpack.system-vars.config.js
+++ webpack.system-vars.config.js
@@ -5,13 +5,12 @@ const DotenvFlowWebpack = require('dotenv-flow-webpack');
 
 module.exports = (_, argv) => {
 	/**
-	 * `dotenv-flow-webpack` plugin basic initialization.
+	 * Initializing `dotenv-flow-webpack` with registering system environment variables.
 	 */
 	const dotenvFlowPlugin = new DotenvFlowWebpack({
 		node_env: (argv.mode && argv.mode !== 'none') ? argv.mode : undefined,
 		default_node_env: 'development',
-		// pattern: 'config/[node_env/].env[.local]',
-		// system_vars: true,
+		system_vars: true,
 		debug: true
 	});
 
